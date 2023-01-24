14444e1ed13723ceae76cd5b2bcf7c8dd71dc36c
AbdurahimBerdiyarov <hoorwen@icloud.com>
2023-01-25 04:00:09 +0900

    feat: develop signup bussines logic

--- train.js
+++ train.js
@@ -1,7 +1,9 @@
-console.log("train ishga tushdi");
+// console.log("train ishga tushdi");
 
-const FootBallPoints = (wins, draws, losse) => {
-  return `${wins * 3 + draws * 1 + losse * 0} `;
-};
-const res = FootBallPoints(3, 4, 2);
-console.log(res);
+// const { Schema } = require("mongoose");
+
+// const FootBallPoints = (wins, draws, losse) => {
+//   return `${wins * 3 + draws * 1 + losse * 0} `;
+// };
+// const res = FootBallPoints(3, 4, 2);
+// console.log(res);
