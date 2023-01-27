37e1e77605ce326cd61934203e9b213c01d0c611
AbdurahimBerdiyarov <hoorwen@icloud.com>
2023-01-28 07:29:42 +0900

    feat: ejs framework orqali frontendi qurishni boshlash

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const router = require("./router.js");
+const router_bssr = require("./router_bssr");
+
+// let session = require("express-session");
+// const MongoDBStore = require("connect-mongodb-session")(session);
+// const store = new MongoDBStore({
+//   uri: process.env.MONGO_URL,
+//   collection: "sessions",
+// });
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -9,13 +17,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2 Session code
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {
+//       maxAge: 1000 * 60 * 30, // for 30 minutes
+//     },
+//     store: store,
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
 // 3 Views
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-// app.use("/resto", router_bssr);   //ananaviy
+app.use("/resto", router_bssr); //ananaviy
 app.use("/", router); //React
 
 module.exports = app;
