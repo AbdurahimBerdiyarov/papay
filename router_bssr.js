11d295e2d0b095b516788aa6cfd5b5079b7e20da
AbdurahimBerdiyarov <hoorwen@icloud.com>
2023-02-01 08:19:34 +0900

    feat: multer single image upload process

--- router_bssr.js
+++ router_bssr.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/productController");
+const { uploadProductImage } = require("./utils/upload-multer");
 
 /****************************
  *        BSSR EJS           *
@@ -18,5 +20,12 @@ router_bssr.get("/logout", restaurantController.logout);
 router_bssr.get("/check-me", restaurantController.checkSessions);
 
 router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.post(
+  "/products/create",
+  restaurantController.validateAuthRestaurant,
+  uploadProductImage.single("product_image"),
+  productController.addNewProduct
+);
+router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
 
 module.exports = router_bssr;
--- train.js
+++ train.js
@@ -7,3 +7,28 @@
 // };
 // const res = FootBallPoints(3, 4, 2);
 // console.log(res);
+
+// const FootBallPoints = (a, b, c) => {
+//   if (a="Number"){
+//     return a
+//   }
+// };
+// const res = FootBallPoints(3, 4, 2);
+// console.log(res);
+// / const func = (a, b, c) => {
+
+// const func = (a, b, c) => {
+//   if (b == "/" && c == 0) {
+//     return "not valid to operate'";
+//   } else if (b == "+") {
+//     return a + c;
+//   } else if (b == "-") {
+//     return a - c;
+//   } else if (b == "*") {
+//     return a * c;
+//   } else if (b == "/") {
+//     return a / c;
+//   }
+// };
+// const war = func("5+5");
+// console.log(war);
